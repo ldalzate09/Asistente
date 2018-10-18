23c68836811ce695954cf8d3f9572aa3cfae1a70
Leydy Diana Alzate <lealzate@leydys-macbook-pro.local>
2018-10-17 19:13:35 -0500

    Initial Commit

---
+++ Asistente/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Asistente
+//
+//  Created by Leydy Alzate on 10/17/18.
+//  Copyright © 2018 Leydy Alzate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
